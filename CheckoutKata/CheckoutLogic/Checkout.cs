fec865fa0fac08a215aa9e4914d0d35dbdce3ae8
Paul Hicks <p.hicks1982@gmail.com>
2019-09-12 22:00:39 +0100

    Implemented Scan method. Although not in the spec, added an Items method to the Checkout class to allow a list of the items to be obtained as this would likely be needed.

--- CheckoutKata/CheckoutLogic/Checkout.cs
+++ CheckoutKata/CheckoutLogic/Checkout.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CheckoutLogic
@@ -9,6 +10,9 @@ namespace CheckoutLogic
 	/// </summary>
 	public class Checkout
 	{
+
+		private List<CheckoutItem> checkoutItems = new List<CheckoutItem>();
+
 		/// <summary>
 		/// The current total of all items that have been scanned
 		/// </summary>
@@ -19,12 +23,37 @@ namespace CheckoutLogic
 		}
 
 		/// <summary>
+		/// Provide access to the items that have been scanned so far.
+		/// </summary>
+		/// <returns></returns>
+		public IList<CheckoutItem> Items()
+		{
+			//return as an array to ensure users of method can't change internal list
+			return this.checkoutItems.ToArray();
+		}
+
+		/// <summary>
 		/// Scan an item into the Checkout
 		/// </summary>
 		/// <param name="item"></param>
 		public void Scan(Item item)
 		{
+			//validation
+			if (item == null)
+			{
+				throw new ArgumentNullException(nameof(item));
+			}
 
+			//lookup the item in the list
+			CheckoutItem checkoutItem = this.checkoutItems.FirstOrDefault(p => p.Sku == item.Sku);
+			if (checkoutItem == null)
+			{
+				//no item with sku was found so create one and add to list (without quantity initially)
+				checkoutItem = new CheckoutItem { Sku = item.Sku }; 
+				this.checkoutItems.Add(checkoutItem);
+			}
+			//increment quantity
+			checkoutItem.Quantity++;
 		}
 	}
 }
---
+++ CheckoutKata/CheckoutLogic/CheckoutItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutLogic
+{
+	/// <summary>
+	/// An item stored in the Checkout
+	/// </summary>
+	public class CheckoutItem
+	{
+		/// <summary>
+		/// The Sku of an item that has been scanned
+		/// </summary>
+		public string Sku { get; set; }
+
+		/// <summary>
+		/// The quantity of an item that has been scanned.
+		/// </summary>
+		public int Quantity { get; set; }
+	}
+}
