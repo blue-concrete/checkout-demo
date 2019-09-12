9cbeb81e130c13cbc79d8a134242f166a84f98a2
Paul Hicks <p.hicks1982@gmail.com>
2019-09-12 21:00:31 +0100

    Creating skeleton of libraries.

---
+++ CheckoutKata/CheckoutLogic/Checkout.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutLogic
+{
+	/// <summary>
+	/// A Checkout that is in progress
+	/// </summary>
+	public class Checkout
+	{
+		/// <summary>
+		/// The current total of all items that have been scanned
+		/// </summary>
+		/// <returns></returns>
+		public decimal Total()
+		{
+			return 0m;
+		}
+
+		/// <summary>
+		/// Scan an item into the Checkout
+		/// </summary>
+		/// <param name="item"></param>
+		public void Scan(Item item)
+		{
+
+		}
+	}
+}
